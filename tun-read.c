b6016063b87a9fbe95b8f44b2a42e34999c4cc52
Etienne Dublé <etienne.duble@gmail.com>
2024-06-15 14:39:03 +0200

    Update tun-read.c & tun-write.c

--- tun-read.c
+++ tun-read.c
@@ -2,19 +2,52 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
+/* Ce programme lit les paquets successifs sur tun_fd=3 (le descripteur
+ * de fichier correspondant à une interface TUN), et copie ces paquets
+ * sur stdout mais en les préfixant par 2 octets précisant leur longueur. */
+
+/* Il faut gérer les possibles écritures partielles sur le flux de sortie
+ * (écriture partielle d'un paquet, ou, si on n'a vraiment pas de chance,
+ * écriture d'un seul octet de longueur au lieu de deux), qui peuvent
+ * survenir quand le buffer de sortie est plein (congestion sur le tunnel
+ * SSH). */
+
 const int tun_fd=3, stdout_fd=1;
 
+int write_n_bytes(char *buf, int length) {
+    int res;
+    char *buf_end = buf + length;
+    while (buf < buf_end) {
+        res = write(stdout_fd, buf, length);
+        if (res <= 0) {
+            return 1;  // error
+        }
+        buf += res;
+        length -= res;
+    }
+    return 0;  // ok
+}
+
 int main() {
-    int length;
+    int length, res;
     uint16_t coded_length;
     char buf[65536];
     while(1) {
+        /* lecture d'un paquet venant du TUN */
         length = read(tun_fd, buf, 65536);
         if (length <= 0) {
             return 1;  // end
         }
+        /* ecriture de 2 octets de longueur sur stdout */
         coded_length = htons((uint16_t)length);
-        write(stdout_fd, &coded_length, 2);
-        write(stdout_fd, buf, length);
+        res = write_n_bytes((char*)&coded_length, 2);
+        if (res != 0) {
+            return 1;  // end
+        }
+        /* ecriture du paquet sur stdout */
+        res = write_n_bytes(buf, length);
+        if (res != 0) {
+            return 1;  // end
+        }
     }
 }
--- tun-write.c
+++ tun-write.c
@@ -2,6 +2,17 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
+/* Ce programme lit un flux de paquets sur son entrée standard, chaque
+ * paquet étant précédé de deux octets indiquant sa longueur, et écrit
+ * ces paquets un par un sur tun_fd=3 (le descripteur de fichier
+ * correspondant à une interface TUN). */
+
+/* Il faut gérer les possibles lectures partielles (lecture partielle
+ * d'un paquet, ou, si on n'a vraiment pas de chance, lecture d'un seul
+ * octet de longueur au lieu de deux), le flux en entrée étant soumis
+ * aux aléas du buffering réseau et à des contraintes de taille maximale
+ * du payload SSH. */
+
 const int tun_fd=3, stdin_fd=0;
 
 int read_n_bytes(char *buf, int length) {
@@ -23,15 +34,18 @@ int main() {
     uint16_t coded_length;
     char buf[65536];
     while(1) {
+        /* lecture de 2 octets de longueur */
         res = read_n_bytes((char*)&coded_length, 2);
         if (res != 0) {
             return 1;  // end
         }
 	    length = (int)ntohs(coded_length);
+        /* lecture du paquet */
         res = read_n_bytes(buf, length);
         if (res != 0) {
             return 1;  // end
         }
+        /* écriture du paquet sur le TUN */
         write(tun_fd, buf, length);
     }
 }
